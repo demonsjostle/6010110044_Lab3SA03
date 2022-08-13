06b3b548a277fe2a112e813734e5d33e89a7141c
KIATISAK SOMKHUNNGUEN <6010110044@psu.ac.th>
2022-08-14 02:48:53 +0700

    handler props

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 export default function CharacterCard(props) {
     const [active, setActive] = useState(false);
     const activate = () => {
-        setActive(true)
+        if (!active) {
+            setActive(true)
+            props.activationHandler(props.value)
+        }
+
+
     }
     const className = `card ${active ? 'activeCard' : ''}`
     return (
